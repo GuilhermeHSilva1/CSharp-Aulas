540e5f73e47deea669204b665ac7204b12c4394a
DESKTOP-6JOQQDV\Guilh <GuilhermeHillesheim387@gmail.com>
2023-06-02 03:11:08 -0300

    Conhecendo o C#

---
+++ Aulas C#/Aula02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Principal{
+    static void Main(string[] args){
+        Console.Write("Olá Mundo...");
+
+        if (args.GetLength(0) > 0){
+            Console.WriteLine(args.GetValue(0));
+        }
+        Console.ReadLine();
+    }
+}
